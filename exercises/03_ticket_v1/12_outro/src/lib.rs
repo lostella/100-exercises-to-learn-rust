ef57c99036a23fdfa9b00d04b8ebd4558dea0e35
Lorenzo Stella <lorenzostella@gmail.com>
2025-01-11 16:02:48 +0100

    complete 03/11-12

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,68 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self {
+            product_name: Self::validate_name(product_name),
+            quantity: Self::validate_quantity(quantity),
+            unit_price: Self::validate_price(unit_price),
+        }
+    }
+
+    fn validate_name(name: String) -> String {
+        if name.len() > 300 || name.is_empty() {
+            panic!("Product name must be non-empty and 300 bytes at most")
+        }
+        name
+    }
+
+    fn validate_quantity(qty: u32) -> u32 {
+        if qty <= 0 {
+            panic!("Quantity must be strictly positive")
+        }
+        qty
+    }
+
+    fn validate_price(price: u32) -> u32 {
+        if price <= 0 {
+            panic!("Price must be strictly positive")
+        }
+        price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = Self::validate_name(product_name)
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = Self::validate_quantity(quantity)
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.unit_price = Self::validate_price(unit_price)
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+}
